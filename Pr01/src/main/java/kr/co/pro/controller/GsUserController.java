7d86a0a268c196068142fd0298803ea0e35b9ce2
JungGunSoo <48461256+JungGunSoo@users.noreply.github.com>
2019-03-20 16:17:00 +0900

    건수작업

--- Pr01/src/main/java/kr/co/pro/controller/GsUserController.java
+++ Pr01/src/main/java/kr/co/pro/controller/GsUserController.java
@@ -1,5 +1,11 @@
 package kr.co.pro.controller;
 
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+@Controller
 public class GsUserController {
-
+	@GetMapping("/asdfasdf")
+	public String dd() {
+		return "asdfnasd";
+	}
 }
